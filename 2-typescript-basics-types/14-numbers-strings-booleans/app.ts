3d26cc03ef8b1674ec9ad460fef973bd1e487eb7
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-14 16:42:59 -0500

    Added numbers, strings, boolean example

---
+++ 2-typescript-basics-types/14-numbers-strings-booleans/app.ts
@@ -0,0 +1,11 @@
+export {};
+
+const add = (n1: number, n2: number, showResults: boolean, phrase: string) => {
+    return showResults ? console.log(`${phrase} ${n1 + n2}`) : n1 + n2; 
+}
+
+const [n1, n2] = [5, 2.8];
+const showResults = true;
+const phrase = 'Result is: ';
+
+add(n1, n2, showResults, phrase);
