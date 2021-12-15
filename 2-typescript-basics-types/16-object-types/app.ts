0b9d942c395e0905a2601ca9a9151e2e9ad2bc7a
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-15 08:22:18 -0500

    Added object types

---
+++ 2-typescript-basics-types/16-object-types/app.ts
@@ -0,0 +1,18 @@
+export {};
+
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Lawrence',
+//     age: 28
+// };
+
+const person = {
+    name: 'Lawrence',
+    age: 28
+}
+
+const {name, age} = person;
+
+console.log(name);
