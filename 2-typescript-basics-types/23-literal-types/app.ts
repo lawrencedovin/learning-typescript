d5781a89148f7bc12643335fdf8264ca42568696
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-16 14:57:22 -0500

    Added literal types example

---
+++ 2-typescript-basics-types/23-literal-types/app.ts
@@ -0,0 +1,17 @@
+export {};
+
+const combine = (input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') => {
+    let result;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') result = (+input1) + (+input2);
+    else result = input1.toString() + input2.toString();
+    return result;
+}
+
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('78', '45', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine(30, 26, 'as-text');
+console.log(combinedNames);
