5122606768c28802bff42db273ad860330226898
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-20 09:05:29 -0500

    Added spread operator example

--- 4-next-gen-js-ts/next-gen-features/dist/app.js
+++ 4-next-gen-js-ts/next-gen-features/dist/app.js
@@ -1,7 +1,14 @@
 "use strict";
-let button = document.querySelector("button");
-function clickHandler(message) {
-    console.log(`Clicked! ${message}`);
-}
-button.addEventListener("click", () => clickHandler('You clicked me.'));
+// const and let are block scoped. Blocks are whatever is in the {}.  
+// lower blocks can also be accessed. Forces cleaner code.
+const username = 'Max';
+// "const" keyword values cannot be changed.
+let age = 30;
+// "let" keyword values allow changes.
+var password = '123abc';
+// "var" keyword is for global and function scopes.
+const hobbies = ['Reading', 'Playing Ukulele'];
+const activeHobbies = ['Walking'];
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
 //# sourceMappingURL=app.js.map
--- 4-next-gen-js-ts/next-gen-features/src/app.ts
+++ 4-next-gen-js-ts/next-gen-features/src/app.ts
@@ -1,9 +1,24 @@
+/**let and const key words*/
+// const and let are block scoped. Blocks are whatever is in the {}.  
+// lower blocks can also be accessed. Forces cleaner code.
 const username = 'Max';
 // "const" keyword values cannot be changed.
 let age = 30;
 // "let" keyword values allow changes.
-
-// const and let are block scoped. Blocks are whatever is in the {}.  
-// lower blocks can also be accessed. Forces cleaner code.
-
+var password = '123abc';
 // "var" keyword is for global and function scopes.
+
+/**Spread Operator*/
+const hobbies = ['Reading', 'Playing Ukulele'];
+const activeHobbies = ['Walking'];
+
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
+// Create a Copy of an Object, not just a pointer that points to an Object in memory.
+const person = {
+    name: 'Max',
+    age: 30
+};
+
+const copiedPerson = {...person};
