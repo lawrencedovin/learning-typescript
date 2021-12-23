6d5d1f59e8cc7a7d444dd0e29741fa2adc3aaee7
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-23 16:22:57 -0500

    Added readonly interface properties

---
+++ 5-classes-interfaces/75-readonly-interface-properties/src/app.ts
@@ -0,0 +1,19 @@
+interface Greetable {
+    readonly name: string;
+    greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+    name: string;
+    age = 30;
+    constructor(name: string) {
+        this.name = name;
+    }
+    greet(phrase: string) {
+        console.log(`${phrase} ${this.name}`);
+    }
+}
+
+let lawrence: Greetable;
+lawrence = new Person('Lawrence');
+lawrence.greet('Hi there - I am');
