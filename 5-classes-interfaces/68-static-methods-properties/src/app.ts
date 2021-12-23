f374468be6256f126fc6f8fcc7b7a447fde86f89
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-23 14:31:09 -0500

    Added static method & propeties example

---
+++ 5-classes-interfaces/68-static-methods-properties/src/app.ts
@@ -0,0 +1,59 @@
+class Department {
+    static fiscalYear = 2020;
+    protected employees: string [] = [];
+
+    constructor(private readonly id: string, public name: string) {
+        console.log(Department.fiscalYear);
+    }
+
+    static createEmployee(name: string) {
+        return { name: name };
+    }
+
+    describe(this: Department) {
+        console.log(`Department: ${this.name}`);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeDetails() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    get mostRecentReport() {
+        if(this.lastReport) return this.lastReport;
+        throw new Error('No report found.');
+    }
+
+    set mostRecentReport(report: string) {
+        if(!report) throw new Error('Please pass in a valid report.');
+        this.addReport(report);
+    }
+
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting');
+        this.lastReport = reports[0];
+    }
+
+    addEmployee(employee: string) {
+        return employee === 'Max' ? null : this.employees.push(employee);
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+}
+
+
+const accounting = new AccountingDepartment('0001', ['First Report', 'Second Report']);
+accounting.addReport('Third Report');
+console.log(accounting.mostRecentReport);
+console.log(Department.fiscalYear);
