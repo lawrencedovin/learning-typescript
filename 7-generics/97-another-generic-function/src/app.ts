d29b1cc728129e7e5eb8a45e0d06082988432488
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-26 17:14:22 -0500

    Added keyof constraint example

--- 7-generics/97-another-generic-function/src/app.ts
+++ 7-generics/97-another-generic-function/src/app.ts
@@ -1,13 +1,9 @@
-interface Lengthy {
-    length: number;
+// Gets an object along with a passed in key.
+// Returns the value of that key in that object.
+// First parameter should be any kind of object.
+// Second parameter is any kind of key that in that object.
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return obj[key];
 }
 
-// Need to pass in a type with a length property like string/ arrays.
-// Not too concerned about which type of data as long as there's a length property.
-function countAndPrint<T extends Lengthy>(element: T): [T, string] {
-    let description = 'Got no value.';
-    if(element.length > 0) description = `Got ${element.length} element/s.`;
-    return [element, description];
-}
-
-console.log(countAndPrint(['Hi', 'there!']));
+extractAndConvert({name: 'Lawrence'}, 'name');
