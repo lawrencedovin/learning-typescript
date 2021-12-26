7a9e2119426e3698ee74100b78f812aecac90225
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-26 14:03:17 -0500

    Added discriminated unions example

---
+++ 6-advanced-types/85-discriminated-unions/src/app.ts
@@ -0,0 +1,28 @@
+const log = (phrase: string) => console.log(phrase);
+
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: 'horse';
+    runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch(animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    log(`Moving at speed: ${speed}/hr`);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10});
