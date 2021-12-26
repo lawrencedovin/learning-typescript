b14f5488da14e3fe3fca4008e73f0a4e5148ff15
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-26 14:43:51 -0500

    Added Index Properties example

--- 6-advanced-types/87-index-properties/src/app.ts
+++ 6-advanced-types/87-index-properties/src/app.ts
@@ -1,65 +1,11 @@
-type Combinable = string | number;
-type Numeric = number | boolean;
-type Universal = Combinable & Numeric;
-
-function add(a: Combinable, b: Combinable) {
-    if(typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
+interface ErrorContainer {
+    // If property doesn't exist like email error but not a username error then omit username.
+    // Gives flexibility for not knowing how many properties and what names ahead of time.
+    // { email: 'Not a valid email.', username: 'Must at least 5 characters long.' }
+    [prop: string]: string;
 }
 
-const log = (phrase: string) => console.log(phrase);
-
-type Employee = {
-    name: string;
-    startDate: Date;
+const errorBag: ErrorContainer = {
+    email: 'Not a valid email.',
+    username: 'Must start with a capital character!'
 };
-
-type Admin = {
-    name: string;
-    privileges: string[];
-};
-
-type UknownEmployee = Employee | Admin;
-
-function printEmployee(employee: UknownEmployee) {
-    log(`Name: ${employee.name}`);
-    if('privileges' in employee) log(`Privileges: ${employee.privileges}`);
-    if('startDate' in employee) log(`Start Date: ${employee.startDate}`);
-}
-
-const employee1: Employee = {
-    name: 'Lawrence',
-    startDate: new Date()
-};
-
-printEmployee(employee1);
-
-class Car {
-    drive() {
-        log('Driving...');
-    }
-}
-
-class Truck {
-    drive() {
-        log('Driving a truck');
-    }
-    loadCargo(amount: number) {
-        log(`Loading cargo... ${amount}`);
-    }
-}
-
-type Vehicle = Car | Truck;
-
-const v1 = new Car();
-const v2 = new Truck();
-
-function useVehicle(vehicle: Vehicle) {
-    vehicle.drive();
-    if(vehicle instanceof Truck) vehicle.loadCargo(1000);
-}
-
-useVehicle(v1);
-useVehicle(v2);
