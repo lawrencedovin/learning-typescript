a4e9ac70a9fb28748b3f17c1a329b02157e52e21
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-26 14:51:38 -0500

    Added function overloads example

--- 6-advanced-types/88-function-overloads/src/app.ts
+++ 6-advanced-types/88-function-overloads/src/app.ts
@@ -2,6 +2,8 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 function add(a: Combinable, b: Combinable) {
     if(typeof a === 'string' || typeof b === 'string') {
         return a.toString() + b.toString();
@@ -9,57 +11,5 @@ function add(a: Combinable, b: Combinable) {
     return a + b;
 }
 
-const log = (phrase: string) => console.log(phrase);
-
-type Employee = {
-    name: string;
-    startDate: Date;
-};
-
-type Admin = {
-    name: string;
-    privileges: string[];
-};
-
-type UknownEmployee = Employee | Admin;
-
-function printEmployee(employee: UknownEmployee) {
-    log(`Name: ${employee.name}`);
-    if('privileges' in employee) log(`Privileges: ${employee.privileges}`);
-    if('startDate' in employee) log(`Start Date: ${employee.startDate}`);
-}
-
-const employee1: Employee = {
-    name: 'Lawrence',
-    startDate: new Date()
-};
-
-printEmployee(employee1);
-
-class Car {
-    drive() {
-        log('Driving...');
-    }
-}
-
-class Truck {
-    drive() {
-        log('Driving a truck');
-    }
-    loadCargo(amount: number) {
-        log(`Loading cargo... ${amount}`);
-    }
-}
-
-type Vehicle = Car | Truck;
-
-const v1 = new Car();
-const v2 = new Truck();
-
-function useVehicle(vehicle: Vehicle) {
-    vehicle.drive();
-    if(vehicle instanceof Truck) vehicle.loadCargo(1000);
-}
-
-useVehicle(v1);
-useVehicle(v2);
+const result = add('Lawrence', ' Dovin');
+result.split(' ');
