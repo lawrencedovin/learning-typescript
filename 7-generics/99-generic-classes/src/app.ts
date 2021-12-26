de0470d60da42fea8681feaf239b4629187cc063
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-26 17:42:52 -0500

    Added generic class example

--- 7-generics/99-generic-classes/src/app.ts
+++ 7-generics/99-generic-classes/src/app.ts
@@ -1,5 +1,29 @@
-const userInput = '';
-// If userInput is null or undefined then use the fall back 'DEFAULT'.
-// Empty string is still allowed
-const storedData = userInput ?? 'DEFAULT';
-console.log(storedData);
+// Makes the class generic which can hold strings, numbers, arrays, object.
+type primitive = string | number | boolean;
+class DataStorage<T extends primitive> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem() {
+        this.data.pop();
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Pizza');
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(312);
+
+// const arrayStringStorage = new DataStorage<Array<string>>();
+// arrayStringStorage.addItem(['hi', 'bye', '123']);
+
+// const objectStorage = new DataStorage<object>();
+// objectStorage.addItem({name: 'Lawrence', age: 28});
