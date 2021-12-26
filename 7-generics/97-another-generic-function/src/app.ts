749951e4dd8c964f9111aadf0089147b6406c8bd
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-26 17:02:51 -0500

    Added another generic function example

--- 7-generics/97-another-generic-function/src/app.ts
+++ 7-generics/97-another-generic-function/src/app.ts
@@ -1,15 +1,13 @@
-type Combinable = string | number;
-type Numeric = number | boolean;
-type Universal = Combinable & Numeric;
-
-function add(a: number, b: number): number;
-function add(a: string, b: string): string;
-function add(a: Combinable, b: Combinable) {
-    if(typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
+interface Lengthy {
+    length: number;
 }
 
-const result = add('Lawrence', ' Dovin');
-result.split(' ');
+// Need to pass in a type with a length property like string/ arrays.
+// Not too concerned about which type of data as long as there's a length property.
+function countAndPrint<T extends Lengthy>(element: T): [T, string] {
+    let description = 'Got no value.';
+    if(element.length > 0) description = `Got ${element.length} element/s.`;
+    return [element, description];
+}
+
+console.log(countAndPrint(['Hi', 'there!']));
