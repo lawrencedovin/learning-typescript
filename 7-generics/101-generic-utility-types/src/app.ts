2e4383cad669491103ed79ab050cdef08f2dff23
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-26 17:56:20 -0500

    Added generic utility types

--- 7-generics/101-generic-utility-types/src/app.ts
+++ 7-generics/101-generic-utility-types/src/app.ts
@@ -1,48 +1,16 @@
-type Admin = {
-    name: string;
-    privileges: string[];
-};
-
-type Employee = {
-    name: string;
-    startDate: Date;
-};
-
-type ElevatedEmployee = Admin & Employee;
-
-const e1: ElevatedEmployee = {
-    name: 'Max',
-    privileges: ['create-server'],
-    startDate: new Date()
+interface CourseGoal {
+    title: string;
+    description: string;
+    completionDate: Date;
 }
 
-// Similar to Interface Inheritance
-
-interface Named {
-    name: string;
+function createCourseGoal(title: string, description: string, completionDate: Date): CourseGoal {
+    // Transforms it into a Partial Type where all the properties are optional
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal = {title, description, completionDate};
+    return courseGoal as CourseGoal;
 }
 
-interface AdminInterface extends Named {
-    privileges: string[];
-}
-
-interface EmployeeInterface extends Named {
-    startDate: Date;
-}
-
-class ElevatedEmployees implements AdminInterface, EmployeeInterface {
-    name: string;
-    privileges: string[];
-    startDate = new Date();
-    
-    constructor(name: string, privileges: string[]) {
-        this.name = name;
-        this.privileges = privileges;
-    }
-}
-
-const el = new ElevatedEmployees('Max', ['create-server']);
-
-type Combinable = string | number;
-type Numeric = number | boolean;
-type Universal = Combinable & Numeric;
+const names: Readonly<string[]> = ['Max', 'Anna'];
+// names.push('Manu');
+// names.pop();
