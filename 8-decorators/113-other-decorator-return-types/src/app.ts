15fe7b94abbe3c662c436292419e9d8e9b3438ca
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-28 15:44:18 -0500

    Added decorator return types notes

--- 8-decorators/113-other-decorator-return-types/src/app.ts
+++ 8-decorators/113-other-decorator-return-types/src/app.ts
@@ -1,9 +1,57 @@
-// Restricting the types of T, U
-// <T extends object, U extends object>: T type can be any object any structure, but needs to be an object.
-
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
+function PropertyDecorator(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
 }
 
-const mergedObject = merge({name: 'Lawrence'}, {age: 28});
-console.log(mergedObject.age);
+function AccessorDecorator(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function MethodDecorator(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+// position is the index of the parameter starting at 0
+function ParameterDecorator(target: any, name: string | Symbol, position: number) {    
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
+class Product {
+    @PropertyDecorator
+    title: string;
+    private _price: number;
+
+    constructor(title: string, price: number) {
+        this.title = title;
+        this._price = price;
+    }
+
+    // target: constructor
+    // name of the setter: price
+    // descriptor: describes the setter
+    @AccessorDecorator
+    set price(value: number) {
+        if(value > 0) this._price = value;
+        else throw new Error('Invalid Price - should be positive number.');
+    }
+
+    // target: constructor
+    // name of method: getPriceWithTax
+    // descriptor: describes the method
+    @MethodDecorator
+    getPriceWithTax(@ParameterDecorator tax: number) {
+        return this._price * (1 + tax);
+    }
+}
+
+// Accessor and Method Decorators return values.
+// Property and Parameter Decorators return values are ignored by TypeScript.
