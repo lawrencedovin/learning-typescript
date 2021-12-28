04f53dd7f1b82610e1867ae57eddcf9a3c6f9823
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-28 15:21:16 -0500

    Added return class in class decorator example

--- 8-decorators/112-return-class-in-class-decorator/src/app.ts
+++ 8-decorators/112-return-class-in-class-decorator/src/app.ts
@@ -1,9 +1,36 @@
-// Gets an object along with a passed in key.
-// Returns the value of that key in that object.
-// First parameter should be any kind of object.
-// Second parameter is any kind of key that in that object.
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
-    return obj[key];
+function Logger(log: string) {
+    return (constructor: Function) => {
+        console.log(log);
+        console.log(constructor);
+    }
 }
 
-extractAndConvert({name: 'Lawrence'}, 'name');
+function WithTemplate(template: string, hookId: string) {
+    return <T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) => {
+        return class extends originalConstructor {
+            constructor(..._: any[]) {
+                //super() saves
+                // name = 'Max';
+                // constructor() {
+                //     console.log('Creating person object...');
+                // }
+                // and adds the extra logic of hookElement etc.
+                // which extends the class
+                super();
+                const hookElement = document.querySelector(`#${hookId}`);
+                if(hookElement) {
+                    hookElement.innerHTML = template;
+                    hookElement.querySelector('h1')!.textContent = this.name;
+                }
+            }
+        }
+    }
+}
+
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class Person {
+    name = 'Max';
+    constructor() {
+        console.log('Creating person object...');
+    }
+}
