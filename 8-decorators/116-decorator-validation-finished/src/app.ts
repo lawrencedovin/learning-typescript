dda177227a9dda5725511afe9d75d91d58d4d642
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-29 11:35:35 -0500

    Added decorator validation finished

--- 8-decorators/116-decorator-validation-finished/src/app.ts
+++ 8-decorators/116-decorator-validation-finished/src/app.ts
@@ -1,10 +1,76 @@
-// A function that merges 2 Objects and returns a Merged Object.
-// objA is any type but different type from objB.
-// Returns the Intersection of them.
-function merge<T, U>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
+interface ValidatorConfig {
+    // Class Name
+    [property: string]: {
+        [validatableProperty: string]: string[] // ['required', 'positive']
+    }
 }
 
-const mergedObj = merge({name: 'Lawrence'}, {age: 28});
+const registeredValidators: ValidatorConfig = {};
 
-console.log(mergedObj.name);
+function Required(target: any, propertyName: string) {
+    registeredValidators[target.constructor.name] = {
+        // Takes already existing property values if it exists ['positive']
+        ...registeredValidators[target.constructor.name],
+        [propertyName]: ['required']
+    };
+}
+
+function PositiveNumber(target: any, propertyName: string) {
+    registeredValidators[target.constructor.name] = {
+        // Takes already existing property values if it exists ['required']
+        ...registeredValidators[target.constructor.name],
+        [propertyName]: ['positive']
+    };
+}
+
+// Runs the logic of the validators
+function validate(obj: any) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    // if no errors for validating that means the form inputs are good.
+    if(!objValidatorConfig) return true;
+    let isValid = true;
+    for(const property in objValidatorConfig) {
+        for(const validator of objValidatorConfig[property]) {
+            switch(validator) {
+                case 'required':
+                    isValid = isValid && !!obj[property];
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[property] > 0;
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
+
+class Course {
+    @Required
+    title: string;
+    @PositiveNumber
+    price: number;
+
+    constructor(title: string, price: number) {
+        this.title = title;
+        this.price = price;
+    }
+}
+
+const courseForm = document.querySelector('form');
+courseForm?.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleElement = document.querySelector('#title') as HTMLInputElement;
+    const priceElement = document.querySelector('#price') as HTMLInputElement;
+
+    const title = titleElement.value;
+    const price = +priceElement.value;
+
+    const createdCourse = new Course(title, price);
+
+    if(!validate(createdCourse)) {
+        alert('Not a valid Course');
+        return;
+    }
+
+    console.log(createdCourse);
+});
